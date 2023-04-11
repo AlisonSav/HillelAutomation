e1491a4e0edd2ed7350b9ae580d7d310533f337d
Alison Sav <alisa.zarytska@troops-ext.fr>
2023-04-11 19:53:09 +0200

    replace 'foundation to 'age'

--- home_work_11_p2.py
+++ home_work_11_p2.py
@@ -2,9 +2,9 @@ from datetime import date
 
 
 class Company:
-    def __init__(self, name: str, foundation: int):
+    def __init__(self, name: str, age: int):
         self.name = name
-        self.foundation = foundation
+        self.age = age
 
     @classmethod
     def company_info(cls, name, year):
@@ -12,5 +12,5 @@ class Company:
 
 
 company_1 = Company.company_info('Google', 1995)
-print(company_1.foundation)
+print(company_1.age)
 
